bc1403a747c2073274e5d94881879632884b20e5
CreatorBailey <baileym1215@gmail.com>
2019-09-05 19:09:18 -0400

    Initial commit

---
+++ src/SongHW.java
@@ -0,0 +1,27 @@
+public class SongHW {
+
+    public static void main(String[] args) {
+        int ShoeNum = 99;
+        String word = "Pairs";
+
+        while (ShoeNum > 0) {
+
+            if (ShoeNum == 1) {
+                word = "Pairs";
+            }
+            System.out.println(ShoeNum + " " + word + " of Shoes On The Wall");
+            System.out.println(ShoeNum + " " + word + " Of Shoes");
+            System.out.println("You Take One Down");
+            System.out.println("Throw Her Around");
+            ShoeNum = ShoeNum - 1;
+
+            if (ShoeNum > 0) {
+                System.out.println(ShoeNum + " " + word + " Pairs Of Shoes On The Wall");
+            } else {
+                System.out.println("No More Pairs Of Shoes On The Wall");
+            }
+        }
+            }
+        }
+
+
